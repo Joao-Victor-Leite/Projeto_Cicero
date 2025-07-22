7a8912d26d10c59308d925fdb8bb1b5bbae8dbfb
Joao-Victor-Leite <jvldsa10@hotmail.com>
2025-07-22 11:55:26 -0300

    cicero_app: criacao da entidade user e seus dados primarios

--- cicero-app/src/main/java/com/duende/cicero_app/model/UserModel.java
+++ cicero-app/src/main/java/com/duende/cicero_app/model/UserModel.java
@@ -1,4 +1,19 @@
 package com.duende.cicero_app.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
+@Entity(name="tb_user")
 public class UserModel {
+
+    @Id
+    private UUID id;
+
+    private String name;
+    private String email;
+    private String password;
 }
