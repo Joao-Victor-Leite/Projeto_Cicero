5f439152b426c6048760aaed37b7f89230138a47
Joao-Victor-Leite <jvldsa10@hotmail.com>
2025-07-22 10:23:09 -0300

    Adiciona arquivos iniciais do projeto Cicero, incluindo configuração do Maven, Dockerfile, classes principais e de teste, e arquivos de configuração.

---
+++ cicero-app/src/main/java/com/duende/cicero_app/CiceroAppApplication.java
@@ -0,0 +1,13 @@
+package com.duende.cicero_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiceroAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiceroAppApplication.class, args);
+	}
+
+}
---
+++ cicero-app/src/test/java/com/duende/cicero_app/CiceroAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duende.cicero_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiceroAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
