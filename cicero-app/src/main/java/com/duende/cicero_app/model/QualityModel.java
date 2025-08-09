a9066a94a0b442859847f741253be0da7188b0f0
João Victor Leite <jvldsa10@hotmail.com>
2025-08-09 00:16:36 -0300

    cicero-app: criação das tabela quality

---
+++ cicero-app/src/main/java/com/duende/cicero_app/model/QualityModel.java
@@ -0,0 +1,32 @@
+package com.duende.cicero_app.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Entity(name = "tb_quality")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class QualityModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Integer cost;
+    private Integer category;
+    private String name;
+
+    @Lob
+    private String description;
+
+    @ManyToMany(mappedBy = "quality")
+    private Set<EquipmentModel> equipment;
+
+}
