e4327e6b54527f64ea1a0cec79704cec37bd7646
João Victor Leite <jvldsa10@hotmail.com>
2025-08-09 00:16:20 -0300

    cicero-app: criação das tabela equipment e sua relacao entre infected e quality

---
+++ cicero-app/src/main/java/com/duende/cicero_app/model/EquipmentModel.java
@@ -0,0 +1,40 @@
+package com.duende.cicero_app.model;
+
+import com.duende.cicero_app.model.infected.InfectedModel;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Entity(name = "tb_equipment")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class EquipmentModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private Integer points;
+    private Integer usesTotal = 4;
+    private Integer usesCurrent = usesTotal;
+    private Integer size = 1;
+
+    @ManyToMany
+    @JoinTable(
+            name = "equipment_quality",
+            joinColumns = @JoinColumn(name = "equipment_id"),
+            inverseJoinColumns = @JoinColumn(name = "quality_id")
+    )
+    private Set<QualityModel> quality;
+
+    @ManyToMany(mappedBy = "equipment")
+    private Set<InfectedModel> infected;
+
+}
