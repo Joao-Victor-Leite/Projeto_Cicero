1db2fece8e180832ffb93d96382023665a6312c7
João Victor Leite <jvldsa10@hotmail.com>
2025-08-14 00:37:40 -0300

    cicero-app: remoção de controllers, services e DTOs para refatoração

--- cicero-app/src/main/java/com/duende/cicero_app/dto/user/UserCreateDTO.java
+++
@@ -1,9 +0,0 @@
-package com.duende.cicero_app.dto.user;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record UserCreateDTO(
-        @NotBlank String name,
-        @NotBlank String email,
-        @NotBlank String password
-) {}
