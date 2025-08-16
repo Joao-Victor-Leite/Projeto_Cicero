4b34063f3235b65329d604d6d80d4d65c6edeceb
João Victor Leite <jvldsa10@hotmail.com>
2025-08-15 23:12:19 -0300

    cicero-app: criação da configuração de itens de qualidade Consumivel

--- cicero-app/src/main/java/com/duende/cicero_app/interfaces/item/ConsumableQualityEffect.java
+++ cicero-app/src/main/java/com/duende/cicero_app/interfaces/item/ConsumableQualityEffect.java
@@ -1,8 +1,15 @@
 package com.duende.cicero_app.interfaces.item;
 
 import com.duende.cicero_app.interfaces.QualityEffectsStrategy;
+import com.duende.cicero_app.model.item.EquipmentModel;
 import org.springframework.stereotype.Component;
 
 @Component("Consumível")
 public class ConsumableQualityEffect implements QualityEffectsStrategy {
+
+    @Override
+    public void apply(EquipmentModel equipment) {
+        equipment.setUsesTotal(1);
+        equipment.setUsesCurrent(equipment.getUsesTotal());
+    }
 }
