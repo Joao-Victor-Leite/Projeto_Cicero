56041308482b4ec0cc365b338a543ad60de2867a
João Victor Leite <jvldsa10@hotmail.com>
2025-08-18 00:36:05 -0300

    cicero-app: criação do DTO de response para a entidade Infected

---
+++ cicero-app/src/main/java/com/duende/cicero_app/dto/infected/InfectedResponseDTO.java
@@ -0,0 +1,13 @@
+package com.duende.cicero_app.dto.infected;
+
+import com.duende.cicero_app.model.infected.InfectedModel;
+
+public record InfectedResponseDTO(
+        String name
+) {
+    public static InfectedResponseDTO fromEntity (InfectedModel infected) {
+        return new InfectedResponseDTO(
+                infected.getName()
+        );
+    }
+}
