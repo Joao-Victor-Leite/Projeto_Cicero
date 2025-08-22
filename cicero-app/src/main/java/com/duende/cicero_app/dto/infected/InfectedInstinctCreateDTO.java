880a7c2b9bcd2017b4c990ffd0a1c568bf3cacfd
João Victor Leite <jvldsa10@hotmail.com>
2025-08-22 00:19:40 -0300

    cicero-app: criação do DTO de create e response de InfectedInstinct

---
+++ cicero-app/src/main/java/com/duende/cicero_app/dto/infected/InfectedInstinctCreateDTO.java
@@ -0,0 +1,8 @@
+package com.duende.cicero_app.dto.infected;
+
+public record InfectedInstinctCreateDTO(
+        Long infectedId,
+        Long instinctId,
+        Integer score
+) {
+}
---
+++ cicero-app/src/main/java/com/duende/cicero_app/dto/infected/InfectedInstinctResponseDTO.java
@@ -0,0 +1,18 @@
+package com.duende.cicero_app.dto.infected;
+
+import com.duende.cicero_app.model.infected.InfectedInstinctModel;
+
+public record InfectedInstinctResponseDTO(
+        Long infectedId,
+        Long instinctId,
+        Integer score
+) {
+
+    public static InfectedInstinctResponseDTO fromEntity (InfectedInstinctModel entity) {
+        return new InfectedInstinctResponseDTO(
+                entity.getInfected().getId(),
+                entity.getInstinct().getId(),
+                entity.getScore()
+        );
+    }
+}
