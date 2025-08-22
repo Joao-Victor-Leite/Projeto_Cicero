31025c304cab5cbbd70c5a82bb88ed0ab1094a9a
João Victor Leite <jvldsa10@hotmail.com>
2025-08-22 00:18:24 -0300

    cicero-app: alteracao do Id do InfectedInstinctRepository

--- cicero-app/src/main/java/com/duende/cicero_app/repository/infected/InfectedInstinctRepository.java
+++ cicero-app/src/main/java/com/duende/cicero_app/repository/infected/InfectedInstinctRepository.java
@@ -1,6 +1,7 @@
 package com.duende.cicero_app.repository.infected;
 
+import com.duende.cicero_app.model.infected.InfectedInstinctId;
 import com.duende.cicero_app.model.infected.InfectedInstinctModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface InfectedInstinctRepository extends JpaRepository <InfectedInstinctModel, Long> {}
+public interface InfectedInstinctRepository extends JpaRepository <InfectedInstinctModel, InfectedInstinctId> {}
