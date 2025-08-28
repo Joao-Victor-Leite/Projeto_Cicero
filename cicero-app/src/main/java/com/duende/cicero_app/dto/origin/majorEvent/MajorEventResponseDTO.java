9b5b777de17966825446a87498e52c9beb662bab
Joao-Victor-Leite <jvldsa10@hotmail.com>
2025-08-28 11:12:00 -0300

    cicero-app: correção de parametros no Response de MajorEvent

--- cicero-app/src/main/java/com/duende/cicero_app/dto/origin/majorEvent/MajorEventResponseDTO.java
+++ cicero-app/src/main/java/com/duende/cicero_app/dto/origin/majorEvent/MajorEventResponseDTO.java
@@ -1,9 +1,9 @@
 package com.duende.cicero_app.dto.origin.majorEvent;
 
-import com.duende.cicero_app.model.origin.OccupationModel;
+import com.duende.cicero_app.model.origin.MajorEventModel;
 
 public record MajorEventResponseDTO (String name) {
-    public static MajorEventResponseDTO fromEntity (OccupationModel occupation) {
-        return new MajorEventResponseDTO(occupation.getName());
+    public static MajorEventResponseDTO fromEntity (MajorEventModel majorEvent) {
+        return new MajorEventResponseDTO(majorEvent.getName());
     }
 }
