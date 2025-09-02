110b0a6320f68f31df349844abc0216cd2f683a2
Joao-Victor-Leite <jvldsa10@hotmail.com>
2025-09-02 13:42:21 -0300

    cicero-app: retorno do nome do instinto em InfectedInstinctResponseDTO

--- cicero-app/src/main/java/com/duende/cicero_app/dto/infected/InfectedInstinctResponseDTO.java
+++ cicero-app/src/main/java/com/duende/cicero_app/dto/infected/InfectedInstinctResponseDTO.java
@@ -5,6 +5,7 @@ import com.duende.cicero_app.model.infected.InfectedInstinctModel;
 public record InfectedInstinctResponseDTO(
         Long infectedId,
         Long instinctId,
+        String instinctName,
         Integer score
 ) {
 
@@ -12,6 +13,7 @@ public record InfectedInstinctResponseDTO(
         return new InfectedInstinctResponseDTO(
                 entity.getInfected().getId(),
                 entity.getInstinct().getId(),
+                entity.getInstinct().getName(),
                 entity.getScore()
         );
     }
