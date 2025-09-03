76d68819c9eb959ceff91d0948fa1f80ed484876
Joao-Victor-Leite <jvldsa10@hotmail.com>
2025-09-03 15:59:21 -0300

    cicero-app: criação do InfectedAttributeService para calculo da vida do personagem

---
+++ cicero-app/src/main/java/com/duende/cicero_app/service/infected/InfectedAttributeService.java
@@ -0,0 +1,27 @@
+package com.duende.cicero_app.service.infected;
+
+import com.duende.cicero_app.model.infected.InfectedInstinctModel;
+import com.duende.cicero_app.model.infected.InfectedModel;
+import org.springframework.stereotype.Service;
+
+@Service
+public class InfectedAttributeService {
+
+    public void calculateHealth (InfectedModel infected) {
+
+        int attributeScore = getAttributeHealthScore(infected);
+        int healthTotal = (attributeScore + 2) * 5;
+
+        infected.setHealthTotal(healthTotal);
+        infected.setHealthCurrent(healthTotal);
+
+    }
+
+    private int getAttributeHealthScore (InfectedModel infected) {
+        return infected.getInstinctList().stream()
+                .filter(i -> i.getInstinct().getName().equalsIgnoreCase(infected.getAttributeHealth().getLabel()))
+                .map(InfectedInstinctModel::getScore)
+                .findFirst()
+                .orElse(0);
+    }
+}
