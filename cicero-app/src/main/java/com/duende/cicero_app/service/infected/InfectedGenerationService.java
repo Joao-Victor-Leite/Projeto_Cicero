0f4306e9a7dbd2f9a9bf9fe36e2b8ab1dde30ef3
Joao-Victor-Leite <jvldsa10@hotmail.com>
2025-09-03 16:03:12 -0300

    cicero-app: atualização do InfectedGenerationService para salvar a geração do personagem

--- cicero-app/src/main/java/com/duende/cicero_app/service/infected/InfectedGenerationService.java
+++ cicero-app/src/main/java/com/duende/cicero_app/service/infected/InfectedGenerationService.java
@@ -3,6 +3,7 @@ package com.duende.cicero_app.service.infected;
 import com.duende.cicero_app.model.infected.InfectedGenerationModel;
 import com.duende.cicero_app.model.infected.InfectedModel;
 import com.duende.cicero_app.model.origin.GenerationModel;
+import com.duende.cicero_app.repository.infected.InfectedGenerationRepository;
 import com.duende.cicero_app.repository.origin.GenerationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,10 +16,14 @@ import java.util.NoSuchElementException;
 public class InfectedGenerationService {
 
     private final GenerationRepository generationRepository;
+    private final InfectedGenerationRepository infectedGenerationRepository;
 
     @Autowired
-    public InfectedGenerationService (GenerationRepository generationRepository) {
+    public InfectedGenerationService (
+            GenerationRepository generationRepository,
+            InfectedGenerationRepository infectedGenerationRepository) {
         this.generationRepository = generationRepository;
+        this.infectedGenerationRepository = infectedGenerationRepository;
     }
 
     public void linkGeneration (InfectedModel infected, short generationId, String description) {
@@ -34,6 +39,9 @@ public class InfectedGenerationService {
         infectedGeneration.setScoreCurrent(infectedGeneration.getScoreTotal());
 
         infected.setGenerationList(new ArrayList<>(List.of(infectedGeneration)));
-        
+
+        infectedGenerationRepository.save(infectedGeneration);
+        infected.getGenerationList().add(infectedGeneration);
+
     }
 }
