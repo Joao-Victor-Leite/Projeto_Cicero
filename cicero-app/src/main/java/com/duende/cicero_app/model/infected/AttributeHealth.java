e353910ec89c456f9976c19b07776a80c0bf5ed0
Joao-Victor-Leite <jvldsa10@hotmail.com>
2025-09-03 16:00:27 -0300

    cicero-app: adição de labels para os atributos de calculo da vida do personagem

--- cicero-app/src/main/java/com/duende/cicero_app/model/infected/AttributeHealth.java
+++ cicero-app/src/main/java/com/duende/cicero_app/model/infected/AttributeHealth.java
@@ -1,6 +1,16 @@
 package com.duende.cicero_app.model.infected;
 
+import lombok.Getter;
+
+@Getter
 public enum AttributeHealth {
-    POTENCIA,
-    RESOLUCAO
+    POTENCIA("Potência"),
+    RESOLUCAO("Resolução");
+
+    private final String label;
+
+    AttributeHealth(String label) {
+        this.label = label;
+    }
+
 }
