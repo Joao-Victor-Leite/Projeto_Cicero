91e2ef104c284fa3d920b8cf8a437b625bbd1b1c
Joao-Victor-Leite <jvldsa10@hotmail.com>
2025-09-03 16:02:35 -0300

    cicero-app: atualização do InfectedInstinctDistributionDTO com o caminho

--- cicero-app/src/main/java/com/duende/cicero_app/dto/infected/InfectedInstinctDistributionDTO.java
+++ cicero-app/src/main/java/com/duende/cicero_app/dto/infected/InfectedInstinctDistributionDTO.java
@@ -1,6 +1,6 @@
 package com.duende.cicero_app.dto.infected;
 
-import com.duende.cicero_app.service.infected.attribute.AttributeDistributionType;
+import com.duende.cicero_app.service.infected.attributeDistribution.AttributeDistributionType;
 
 import java.util.Map;
 
