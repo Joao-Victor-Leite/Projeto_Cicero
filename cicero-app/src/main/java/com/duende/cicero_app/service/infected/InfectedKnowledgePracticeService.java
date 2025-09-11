9d9e62ea8a0626f8bc2b15a0ff0d45cb5682d5a8
Joao-Victor-Leite <jvldsa10@hotmail.com>
2025-09-11 12:08:22 -0300

    cicero-app: criação de InfectedKnowledgePracticeService

---
+++ cicero-app/src/main/java/com/duende/cicero_app/service/infected/InfectedKnowledgePracticeService.java
@@ -0,0 +1,62 @@
+package com.duende.cicero_app.service.infected;
+
+import com.duende.cicero_app.dto.infected.InfectedKnowledgePracticeDistributionDTO;
+import com.duende.cicero_app.dto.infected.InfectedKnowledgePracticeResponseDTO;
+import com.duende.cicero_app.model.attribute.KnowledgeModel;
+import com.duende.cicero_app.model.attribute.PracticeModel;
+import com.duende.cicero_app.model.infected.InfectedModel;
+import com.duende.cicero_app.repository.attribute.KnowledgeRepository;
+import com.duende.cicero_app.repository.attribute.PracticeRepository;
+import com.duende.cicero_app.repository.infected.InfectedKnowledgeRepository;
+import com.duende.cicero_app.repository.infected.InfectedPracticeRepository;
+import com.duende.cicero_app.repository.infected.InfectedRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+@Service
+public class InfectedKnowledgePracticeService {
+
+    private final InfectedRepository infectedRepository;
+    private final KnowledgeRepository knowledgeRepository;
+    private final PracticeRepository practiceRepository;
+    private final InfectedKnowledgeRepository infectedKnowledgeRepository;
+    private final InfectedPracticeRepository infectedPracticeRepository;
+
+    @Autowired
+    public InfectedKnowledgePracticeService (
+            InfectedRepository infectedRepository,
+            KnowledgeRepository knowledgeRepository,
+            PracticeRepository practiceRepository,
+            InfectedKnowledgeRepository infectedKnowledgeRepository,
+            InfectedPracticeRepository infectedPracticeRepository
+    ) {
+        this.infectedRepository = infectedRepository;
+        this.knowledgeRepository = knowledgeRepository;
+        this.practiceRepository = practiceRepository;
+        this.infectedKnowledgeRepository = infectedKnowledgeRepository;
+        this.infectedPracticeRepository = infectedPracticeRepository;
+    }
+
+    public List<InfectedKnowledgePracticeResponseDTO> applyKnowledgePracticePoints (InfectedKnowledgePracticeDistributionDTO dto) {
+        InfectedModel infected = infectedRepository.findById(dto.InfectedId())
+                .orElseThrow(() -> new NoSuchElementException("Infected não encontrado"));
+
+        List<KnowledgeModel> knowledge = knowledgeRepository.findAll();
+        if (knowledge.size() != 6) {
+            throw new IllegalStateException("Esperado exatamente 6 Conhecimentos cadastrados.");
+        }
+
+        List<PracticeModel> practice = practiceRepository.findAll();
+        if (practice.size() != 6) {
+            throw new IllegalStateException("Esperado exatamente 6 Práticas cadastrados.");
+        }
+
+        return responseList;
+    }
+
+
+
+}
