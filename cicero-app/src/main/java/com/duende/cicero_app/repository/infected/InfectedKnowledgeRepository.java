39cd07b58b6f996501f111de7159f0dc06cd206c
Joao-Victor-Leite <jvldsa10@hotmail.com>
2025-09-15 12:33:42 -0300

    cicero-app: criação de métodos de listagem do infectado e encontrar pelo infected e knowledge/practice para os seus Repositories

--- cicero-app/src/main/java/com/duende/cicero_app/repository/infected/InfectedKnowledgeRepository.java
+++ cicero-app/src/main/java/com/duende/cicero_app/repository/infected/InfectedKnowledgeRepository.java
@@ -1,6 +1,17 @@
 package com.duende.cicero_app.repository.infected;
 
+import com.duende.cicero_app.model.attribute.KnowledgeModel;
 import com.duende.cicero_app.model.infected.InfectedKnowledgeModel;
+import com.duende.cicero_app.model.infected.InfectedModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface InfectedKnowledgeRepository extends JpaRepository <InfectedKnowledgeModel, Long> {}
+import java.util.List;
+import java.util.Optional;
+
+public interface InfectedKnowledgeRepository extends JpaRepository <InfectedKnowledgeModel, Long> {
+
+    Optional<InfectedKnowledgeModel> findByInfectedAndKnowledge (InfectedModel infected, KnowledgeModel knowledge);
+
+    List<InfectedKnowledgeModel> findByInfected (InfectedModel infected);
+
+}
--- cicero-app/src/main/java/com/duende/cicero_app/repository/infected/InfectedPracticeRepository.java
+++ cicero-app/src/main/java/com/duende/cicero_app/repository/infected/InfectedPracticeRepository.java
@@ -1,6 +1,17 @@
 package com.duende.cicero_app.repository.infected;
 
+import com.duende.cicero_app.model.attribute.PracticeModel;
+import com.duende.cicero_app.model.infected.InfectedModel;
 import com.duende.cicero_app.model.infected.InfectedPracticeModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface InfectedPracticeRepository extends JpaRepository <InfectedPracticeModel, Long> {}
+import java.util.List;
+import java.util.Optional;
+
+public interface InfectedPracticeRepository extends JpaRepository <InfectedPracticeModel, Long> {
+
+    Optional<InfectedPracticeModel> findByInfectedAndPractice(InfectedModel infected, PracticeModel practice);
+
+    List<InfectedPracticeModel> findByInfected(InfectedModel infected);
+
+}
